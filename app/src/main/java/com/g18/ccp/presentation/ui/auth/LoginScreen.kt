b2d36724e7f9961111ee13a3670b995ce5300717
BrayanRGarciaM2 <br.garciam1@uniandes.edu.co>
2025-04-03 00:46:02 -0500

    WIP: Implement LoginScreen UI with email and password fields, back button, and login action

--- app/src/main/java/com/g18/ccp/presentation/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/g18/ccp/presentation/ui/auth/LoginScreen.kt
@@ -1,8 +1,165 @@
 package com.g18.ccp.presentation.ui.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.getString
+import com.g18.ccp.R
+import com.g18.ccp.presentation.theme.BackgroundColor
+import com.g18.ccp.presentation.theme.ButtonBackgroundColor
+import com.g18.ccp.presentation.theme.SecondaryColor
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(
+    onBackClick: () -> Unit = {},
+    onLoginClick: () -> Unit = {}
+) {
+    val backgroundColor = SecondaryColor
+    val buttonColor = ButtonBackgroundColor
+    val textFieldColor = Color.White
+    val context = LocalContext.current
+    var email by remember { mutableStateOf("") }
 
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = backgroundColor
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            // Botón de retroceso
+            IconButton(
+                onClick = onBackClick,
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(top = 55.dp, start = 16.dp)
+                    .size(35.dp)
+            ) {
+                Icon(
+                    modifier = Modifier.size(35.dp),
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    contentDescription = "Back",
+                    tint = BackgroundColor
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        horizontal = 32.dp,
+                        vertical = 190.dp
+                    ),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Título CCP
+                Text(
+                    text = getString(context, R.string.app_name),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = BackgroundColor
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                // Campo Correo
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = {
+                        email = it
+                    },
+                    label = {
+                        Text(
+                            modifier = Modifier.background(Color.Transparent),
+                            text = getString(context, R.string.email_text),
+                            style = MaterialTheme.typography.labelMedium,
+                        )
+                    },
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Transparent),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.Gray,
+                        focusedLabelColor = BackgroundColor,
+                        unfocusedLabelColor = Color.Black,
+                        unfocusedBorderColor = Color.Transparent,
+                        focusedBorderColor = Color.Transparent,
+                        cursorColor = Color.Black,
+                        focusedContainerColor = textFieldColor,
+                        unfocusedContainerColor = textFieldColor
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Campo Contraseña
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text("Contraseña") },
+                    singleLine = true,
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Transparent),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.Black,
+                        focusedLabelColor = Color.Black,
+                        unfocusedLabelColor = Color.Black,
+                        unfocusedBorderColor = Color.Transparent,
+                        focusedBorderColor = Color.Transparent,
+                        cursorColor = Color.Black,
+                        focusedContainerColor = textFieldColor,
+                        unfocusedContainerColor = textFieldColor
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                // Botón Acceder
+                Button(
+                    onClick = onLoginClick,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(48.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = buttonColor,
+                        contentColor = Color.Black
+                    )
+                ) {
+                    Text("Acceder")
+                }
+            }
+        }
+    }
 }
