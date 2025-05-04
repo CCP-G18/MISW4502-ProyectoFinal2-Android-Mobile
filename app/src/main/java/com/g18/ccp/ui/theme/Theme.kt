479f6a4663004b930db777c38f7a31f0d41a88a7
BrayanRGarciaM2 <br.garciam1@uniandes.edu.co>
2025-05-04 13:43:31 -0500

    Sort customer list by name and simplify theme color scheme

--- app/src/main/java/com/g18/ccp/ui/theme/Theme.kt
+++ app/src/main/java/com/g18/ccp/ui/theme/Theme.kt
@@ -1,14 +1,10 @@
 package com.g18.ccp.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = MainColor,
@@ -41,15 +37,7 @@ fun CCPTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = DarkColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
